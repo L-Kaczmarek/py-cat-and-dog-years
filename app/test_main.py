c39a7a115f278b8215498c9308e3b415c38db11a
Łukasz Kaczmarek <Lukasz.kaczmarek1993@wp.pl>
2026-04-05 21:17:31 +0200

    'solution2'

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,25 @@
 from app.main import get_human_age
 
 
-def test_get_human_age_less_then_15() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-    assert get_human_age(27, 27) == [2, 2]
-    assert get_human_age(28, 28) == [3, 2]
-    assert get_human_age(100, 100) == [21, 17]
-    assert get_human_age(15, 15) == [1, 1]
-    assert get_human_age(23, 23) == [1, 1]
-    assert get_human_age(24, 24) == [2, 2]
-    assert get_human_age(0, 0) == [0, 0]
-    assert get_human_age(-10, -10) == [0, 0]
+import pytest
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (14, 14, [0, 0]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (0, 0, [0, 0]),
+        (-10, -10, [0, 0]),
+    ],
+)
+def test_get_human_age(cat_age: int,
+                       dog_age: int,
+                       expected: list[int]
+                       ) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
