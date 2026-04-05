ad557576491fbc2c7276383188a76bd8b1763cda
Łukasz Kaczmarek <Lukasz.kaczmarek1993@wp.pl>
2026-04-05 21:00:01 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_get_human_age_less_then_15() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_get_human_age_beetween_15_27() -> None:
+    assert get_human_age(27, 27) == [2, 2]
+
+
+def test_get_human_age_more_than_27() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_get_human_age_100() -> None:
+    assert get_human_age(100, 100) == [21, 17]
